d0b9670e6b657f68e461fd747763b906bc98ca38
Laura PAdrón <laupadron14528@gmail.com>
2024-03-10 16:37:08 -0300

    oak add

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-Deno.serve(() => new Response("Hello, world!"));
+import { Application } from "https://deno.land/x/oak@14.2.0/mod.ts";
+
+const app = new Application();
+
+app.use((ctx) => {
+  ctx.response.body = "Hello World with Oak!";
+});
+
+await app.listen({ port: 8000 });
