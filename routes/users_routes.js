fab9f6c91a53969712cec169b8605f2e2eafa8f4
Laura PAdrón <laupadron14528@gmail.com>
2024-03-10 16:54:35 -0300

    prueba routes

--- routes/users_routes.js
+++ routes/users_routes.js
@@ -2,6 +2,8 @@ import {  Router } from "https://deno.land/x/oak@14.2.0/mod.ts";
 
 const router = Router();
 
-router.get('/users', getUsers);
+router.get('/users', getUsers, (ctx)=>{
+    ctx.response.body = "Hello users!";
+  });
 
 export default router;
