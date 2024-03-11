88ad1aafa5d7dd5e95106a97fbb14caf8e8e5ba7
Laura PAdrón <laupadron14528@gmail.com>
2024-03-11 20:01:31 -0300

    POSTMAN NEON

--- src/db.ts
+++ src/db.ts
@@ -1,7 +1,15 @@
 // db.ts
 import { Pool } from "https://deno.land/x/postgres@v0.17.0/mod.ts";
 
-const databaseUrl = Deno.env.get("DATABASE_URL")!;
+// Obtén la cadena de conexión de la variable de entorno "DATABASE_URL"
+const databaseUrl = Deno.env.get("DATABASE_URL");
+
+// Verifica si la variable de entorno está definida
+if (!databaseUrl) {
+  throw new Error("La variable de entorno DATABASE_URL no está definida.");
+}
+
+// Crea el pool de conexión a la base de datos
 const pool = new Pool(databaseUrl, 3, true);
 
 export default pool;
