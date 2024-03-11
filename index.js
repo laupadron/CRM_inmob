d7f0d1265b42226f62fe9dfc5dc8a05d2e5f82a2
Laura PAdrón <laupadron14528@gmail.com>
2024-03-11 10:13:23 -0300

    prueba routes

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 import { Application } from "https://deno.land/x/oak@14.2.0/mod.ts";
-import usersRoutes from './routes/users_routes.js'
+import usersRoutes from './routes/users_routes.js';
+
 
 const app = new Application();
 
 app.use(usersRoutes.routes())
 
+app.listen(3000, () =>{
+    console.log('server is running')
+})
 
 
 await app.listen({ port: 8000 });
