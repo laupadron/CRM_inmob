f9abc4290553ed3077d78cdb605bafbd5dafa560
Laura PAdrón <laupadron14528@gmail.com>
2024-03-11 19:51:10 -0300

    POSTMAN NEON

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { Application } from "https://deno.land/x/oak@14.2.0/mod.ts";
 import router from './routes/routes.ts'
 
-
+console.log("Server is starting...");
 const app = new Application();
 
 app.use(router.routes())
